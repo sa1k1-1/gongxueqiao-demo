7eddd97bc3660023499e1dcc576b5cc413e6122b
大浪航海 <15339444+dalanghanghai@user.noreply.gitee.com>
2025-11-08 11:26:04 +0800

    Configure base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,12 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+
+  // --- 这是你需要添加的代码 ---
+  // 告诉 Vite，你的项目是部署在 /gongxueqiao-demo/ 这个子目录下的
+  base: '/gongxueqiao-demo/',
+  // -------------------------
+
   plugins: [
     vue(),
     vueDevTools(),
